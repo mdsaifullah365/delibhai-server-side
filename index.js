207b2e39f423416a4643e8bb8a32529dde1c4c5d
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2022-06-04 04:09:34 +0600

    connect mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// MongoDB
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.zbkdy.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("delibhai").collection("users");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Port Listening
 app.listen(port, () => {
